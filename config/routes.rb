380f64e57118d53b8cb21cceac2381335cae501e
eebbesen <eebbesen.git@gmail.com>
2020-05-31 22:10:38 -0500

    default to visits

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'customers#index'
+  root to: 'visits#index'
 end
